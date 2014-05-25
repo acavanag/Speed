4e906bcd0a7ed001682ce6081f99f446a73da2dc
acavanaghww <acavanagh@weddingwire.com>
2014-05-25 09:42:58 -0400

    Initial Commit

---
+++ SpeedTests/SpeedTests.m
@@ -0,0 +1,34 @@
+//
+//  SpeedTests.m
+//  SpeedTests
+//
+//  Created by Andrew Cavanagh on 5/25/14.
+//  Copyright (c) 2014 WeddingWire. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SpeedTests : XCTestCase
+
+@end
+
+@implementation SpeedTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
